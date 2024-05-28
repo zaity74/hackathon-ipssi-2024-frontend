fd1fc8decdfe898d1fe340b86a839aabd2b9902b
nmcoulibaly <ndeyembarcoulibaly@gmail.com>
2024-05-28 17:06:37 +0200

    complet home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import Menu from "../../components/menu/Menu";
 import Header from "../../components/header/Header";
 import "./Home.css"
+import Card from "../../components/card/Card";
+import Footer from "../../components/footer/Footer";
 
 
 const Home = () => {
@@ -32,6 +34,37 @@ const Home = () => {
                     each sport, sport popularity, and the relationship between Olympic Medal counts and country’s GDP.
                 </p>
             </div>
+            <div id="team">
+                <img className="imgTeam" src="https://olympics.com/athlete365/app/uploads/2023/01/FieldOfPlay-scaled.jpg" />
+                <div id="teamContainer">
+                    <h3 className="teamNames">Team 13</h3>
+                    <div className="cardContainer">
+                        <Card />
+                    </div>
+                </div>
+            </div>
+            <div id="projectFeedback">
+                <div className="sectionTitle">
+                    <h3 className="titleFeedback"> Feedback du projet</h3>
+                    <img className="logoJO" src="https://i.pinimg.com/originals/c0/11/a5/c011a51ad84937936e9af692d95b2e2a.png" />
+                </div>
+                <p className="descriptionFeedback"> 1. Analyze and create visualizations on an Olympic dataset from 1896 to 2016 to uncover which countries
+                    stay at the top overtime, by season, and by sport; what it takes for an Olympian to be at the top for
+                    each sport, sport popularity, and the relationship between Olympic Medal counts and country’s GDP.
+                    1. Analyze and create visualizations on an Olympic dataset from 1896 to 2016 to uncover which countries
+                    stay at the top overtime, by season, and by sport; what it takes for an Olympian to be at the top for
+                    each sport, sport popularity, and the relationship between Olympic Medal counts and country’s GDP.
+                </p>
+                <p className="descriptionFeedback"> 1. Analyze and create visualizations on an Olympic dataset from 1896 to 2016 to uncover which countries
+                    stay at the top overtime, by season, and by sport; what it takes for an Olympian to be at the top for
+                    each sport, sport popularity, and the relationship between Olympic Medal counts and country’s GDP.
+                    1. Analyze and create visualizations on an Olympic dataset from 1896 to 2016 to uncover which countries
+                    stay at the top overtime, by season, and by sport; what it takes for an Olympian to be at the top for
+                    each sport, sport popularity, and the relationship between Olympic Medal counts and country’s GDP.
+                </p>
+            </div>
+            <Footer />
+
         </>)
 }
 export default Home;
